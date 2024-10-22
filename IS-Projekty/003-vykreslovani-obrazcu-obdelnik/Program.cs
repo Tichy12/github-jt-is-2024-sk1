2fdc40f89ac79128eb52fcdae558fb46921b7f47
Jan Tichý <jan.tichy1@tul.cz>
2024-10-22 22:29:59 +0200

    #003-vykreslovani-obrazcu

---
+++ IS-Projekty/003-vykreslovani-obrazcu-obdelnik/Program.cs
@@ -0,0 +1,54 @@
+﻿﻿string again = "a";
+while(again == "a") {
+
+            Console.Clear();
+            Console.WriteLine("****************************");
+            Console.WriteLine("********* Obdelník *********");
+            Console.WriteLine("********* Jan Tichý ********");
+            Console.WriteLine("****************************");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.Write("Zadejte číslo (celé číslo): ");
+            uint width;
+            while(!uint.TryParse(Console.ReadLine(), out width)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte první číslo znovu: ");
+            }
+
+            Console.Write("Zadejte číslo (celé číslo): ");
+            uint height;
+            while(!uint.TryParse(Console.ReadLine(), out height)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte první číslo znovu: ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("========================================================");
+            Console.WriteLine("Zadané hodnoty: ");
+            Console.WriteLine("Šířka: {0}", width);
+            Console.WriteLine("výška: {0}", height);
+            Console.WriteLine("========================================================");
+
+            // for( int i = 1; i<=10 ;i++){
+            //     Console.WriteLine(i);
+            // }
+
+            // int j = 1;
+            // while (j <= 10) {
+            //     Console.WriteLine(j);
+            //     j++;
+            // }
+          
+            for(int i = 1; i <= height ;i++){
+                for(int j = 1; j <= width ;j++){
+                   Console.Write("* "); 
+                   System.Threading.Thread.Sleep(System.TimeSpan.FromMilliseconds(100));
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Program můžete opakovat stiskem klávesy 'a'");
+            again = Console.ReadLine(); 
+
+}
