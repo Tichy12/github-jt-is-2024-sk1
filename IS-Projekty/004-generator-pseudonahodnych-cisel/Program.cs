bc5100352e878db4c1fbdae9ed5656da739e8fe5
Jan Tichý <jan.tichy1@tul.cz>
2024-10-23 10:08:13 +0200

    #004-update-2

--- IS-Projekty/004-generator-pseudonahodnych-cisel/Program.cs
+++ IS-Projekty/004-generator-pseudonahodnych-cisel/Program.cs
@@ -34,25 +34,40 @@
             Console.WriteLine("==========================================");
             Console.WriteLine();
 
-            //deklarace pole
+            //deklarace pole    
             int[] myArray = new int[n];
 
             Random randomNumber = new Random();
 
+            int kladna = 0;
+            int zaporna = 0;
+            int nuly = 0;
+
             Console.WriteLine("\n\nNáhodná čísla:");
             for(int i=0; i<n; i++) {
                 myArray[i] = randomNumber.Next(dm, hm+1);
-                Console.WriteLine("{0}; ", myArray[i]);
+                Console.Write("{0}; ", myArray[i]); 
 
+            //    if(myArray[i]>0)
+            //        kladna++;
+            //    if(myArray[i]<0)
+            //        zaporna++;
+            //    if(myArray[i] ==0)
+            //        nuly++;
+
+            if(myArray[i]>0)
+                kladna++;
+            else if (myArray[i]<0)
+                zaporna++;
+            else
+                nuly++;
             }
-
-
+        
+        Console.WriteLine();
+        Console.WriteLine("Počet kladných čísel: {0}", kladna);
+        Console.WriteLine("Počet záporných čísel: {0}", zaporna);
+        Console.WriteLine("Počet nul: {0}", nuly);
             
-
-
-
-
-
             Console.WriteLine();
             Console.WriteLine("Pro opakování programu stiskněte klávesu A");
             again = Console.ReadLine();
